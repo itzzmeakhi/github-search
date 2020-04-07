791b0f5644f05f39615ae13d72a570d98072d8a8
itzzmeakhi <akhilmallidi.98@gmail.com>
2020-04-07 22:15:05 +0530

    Updates

---
+++ precache-manifest.b004382996b596a83c1bf218df5c2cc4.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c49f367848dd627c817c99c824e19914",
+    "url": "/github-pages/index.html"
+  },
+  {
+    "revision": "bea658c1d979569888bb",
+    "url": "/github-pages/static/css/2.48657cd9.chunk.css"
+  },
+  {
+    "revision": "29b2d08e44e0086a3304",
+    "url": "/github-pages/static/css/main.819b025d.chunk.css"
+  },
+  {
+    "revision": "bea658c1d979569888bb",
+    "url": "/github-pages/static/js/2.4770c808.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github-pages/static/js/2.4770c808.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29b2d08e44e0086a3304",
+    "url": "/github-pages/static/js/main.7994a953.chunk.js"
+  },
+  {
+    "revision": "ce6f6e31814eb2660b65",
+    "url": "/github-pages/static/js/runtime-main.30b0d46b.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/github-pages/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/github-pages/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/github-pages/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/github-pages/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/github-pages/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
